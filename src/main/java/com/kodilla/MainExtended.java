e6df5c32ae0b1a83b12a90baae6d61a0f2a6920d
aleksma7 <143549406+aleksma7@users.noreply.github.com>
2023-09-02 11:53:09 +0200

    Add files via upload

---
+++ src/main/java/com/kodilla/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String [] args) {
+        String myName = "Aleksandra";
+        int yearOfBirth = 1999;
+        int numberOfCats = 3;
+        char drivingLicense = 'B';
+        boolean isTooAmbitious = true;
+        double height = 1.65;
+        System.out.println(myName);
+        System.out.println(yearOfBirth);
+        System.out.println(numberOfCats);
+        System.out.println(drivingLicense);
+        System.out.println(isTooAmbitious);
+        System.out.println(height);
+    }
+}
