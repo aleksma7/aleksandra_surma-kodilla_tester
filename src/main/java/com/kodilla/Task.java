2c3841aafd6d5e5067607b097ab99ef2ea4a7098
aleksma7 <143549406+aleksma7@users.noreply.github.com>
2023-11-03 10:15:16 +0100

    Add files via upload

---
+++ src/main/java/com/kodilla/Task.java
@@ -0,0 +1,8 @@
+package com.kodilla.collections.adv.immutable.homework;
+
+public class Task {
+    public static void main(String[] args) {
+        Info info = new Info("TiO2 synthesis", 2);
+        TaskRecord taskRecord = new TaskRecord("Photocatalysis", 4, 2023);
+    }
+}
