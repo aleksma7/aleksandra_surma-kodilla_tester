151325a5a56b7ee064a87ad7878106e3f9934cab
aleksma7 <143549406+aleksma7@users.noreply.github.com>
2023-11-21 23:46:18 +0100

    Add files via upload

--- src/main/java/com/kodilla/UsersValidatorTest.java
+++ src/main/java/com/kodilla/UsersValidatorTest.java
@@ -10,39 +10,24 @@ public class UsersValidatorTest {
 
     @Test
     public void shouldReturnTrueForValidEmail() {
-        // Arrange
         UsersValidator validator = new UsersValidator();
-        String validEmail = "test@example.com";
-
-        // Act
+        String validEmail = "example@example.com";
         boolean result = validator.validateEmail(validEmail);
-
-        // Assert
         assertTrue(result);
     }
 
     @Test
     public void shouldReturnFalseForInvalidEmail() {
-        // Arrange
         UsersValidator validator = new UsersValidator();
         String invalidEmail = "invalid.email";
-
-        // Act
         boolean result = validator.validateEmail(invalidEmail);
-
-        // Assert
         assertFalse(result);
     }
 
     @Test
     public void shouldReturnFalseForNullEmail() {
-        // Arrange
         UsersValidator validator = new UsersValidator();
-
-        // Act
         boolean result = validator.validateEmail(null);
-
-        // Assert
         assertFalse(result);
     }
 
