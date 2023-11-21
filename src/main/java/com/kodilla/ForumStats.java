d0c6c5e3363a9303e077116fde09b788ff787e44
aleksma7 <143549406+aleksma7@users.noreply.github.com>
2023-11-21 10:21:10 +0100

    Add files via upload

---
+++ src/main/java/com/kodilla/ForumStats.java
@@ -0,0 +1,37 @@
+package com.kodilla.stream.homework;
+
+import com.kodilla.stream.User;
+import com.kodilla.stream.UsersRepository;
+
+import java.util.List;
+
+public class ForumStats {
+
+    public double getAveragePostCountForUsersOver40(List<User> users) {
+        return users.stream()
+                .filter(user -> user.getAge() >= 40)
+                .mapToInt(User::getNumberOfPost)
+                .average()
+                .orElse(0.0);
+    }
+
+    public double getAveragePostCountForUsersUnder40(List<User> users) {
+        return users.stream()
+                .filter(user -> user.getAge() < 40)
+                .mapToInt(User::getNumberOfPost)
+                .average()
+                .orElse(0.0);
+    }
+
+    public static void main(String[] args) {
+        List<User> users = UsersRepository.getUsersList();
+
+        ForumStats forumStats = new ForumStats();
+
+        double averagePostsAbove40 = forumStats.getAveragePostCountForUsersOver40(users);
+        double averagePostsBelow40 = forumStats.getAveragePostCountForUsersUnder40(users);
+
+        System.out.println("Średnia liczba postów dla użytkowników >= 40 lat: " + averagePostsAbove40);
+        System.out.println("Średnia liczba postów dla użytkowników < 40 lat: " + averagePostsBelow40);
+    }
+}
