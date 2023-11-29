c9f0c18bb8ad9ef5e36e579f2ebf75c51e62a8db
aleksma7 <143549406+aleksma7@users.noreply.github.com>
2023-11-29 11:41:38 +0100

    Add files via upload

--- src/main/java/com/kodilla/Person.java
+++ src/main/java/com/kodilla/Person.java
@@ -1,27 +1,38 @@
-package com.kodilla;
+package com.kodilla.parametrized_tests.homework;
 
 public class Person {
-    private String name;
-    private double age;
-    private double height;
+    private double heightInMeters;
+    private double weightInKilogram;
 
-    public Person(String name, double age, double height) {
-        this.name = name;
-        this.age = age;
-        this.height = height;
+    public Person(double heightInMeters, double weightInKilogram) {
+        this.heightInMeters = heightInMeters;
+        this.weightInKilogram = weightInKilogram;
     }
 
-    public boolean isOlderThan30() {
-        return age > 30;
-    }
-
-    public boolean isTallerThan160() {
-        return height > 160;
-    }
-
-    public void showPersonData() {
-        System.out.println("Name:" + " " + name);
-        System.out.println("Age:" + " " + age);
-        System.out.println("Height:" + " " + height);
+    public String getBMI() {
+        double bmi = weightInKilogram / (heightInMeters * heightInMeters);
+        if (bmi < 15) {
+            return "Very severely underweight";
+        } else if (bmi < 16) {
+            return "Severely underweight";
+        } else if (bmi < 18.5) {
+            return "Underweight";
+        } else if (bmi < 25) {
+            return "Normal (healthy weight)";
+        } else if (bmi < 30) {
+            return "Overweight";
+        } else if (bmi < 35) {
+            return "Obese Class I (Moderately obese)";
+        } else if (bmi < 40) {
+            return "Obese Class II (Severely obese)";
+        } else if (bmi < 45) {
+            return "Obese Class III (Very severely obese)";
+        } else if (bmi < 50) {
+            return "Obese Class IV (Morbidly Obese)";
+        } else if (bmi < 60) {
+            return "Obese Class V (Super Obese)";
+        } else {
+            return "Obese Class VI (Hyper Obese)";
+        }
     }
 }
