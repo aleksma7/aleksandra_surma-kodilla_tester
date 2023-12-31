74be8bcc6f8397f55c563d4b93cfd49e6a1d602d
aleksma7 <143549406+aleksma7@users.noreply.github.com>
2023-12-31 07:31:34 +0100

    GamblingMachineTest.java

--- src/main/java/com/kodilla/GamblingMachineTest.java
+++ src/main/java/com/kodilla/GamblingMachineTest.java
@@ -3,6 +3,10 @@ package parametrized_tests;
 import com.kodilla.parametrized_tests.homework.GamblingMachine;
 import com.kodilla.parametrized_tests.homework.InvalidNumbersException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -10,34 +14,39 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GamblingMachineTest {
-    @Test
-    void testHowManyWins() throws InvalidNumbersException {
+    @ParameterizedTest
+    @CsvFileSource(resources = "gamblingMachineNumberOfElements.csv")
+    public void shouldThrowExceptionWhenWrongNumberOfNumbers(String input) {
+        String[] numbersAsStrings = input.split("-");
+        Set<Integer> inputSet = new HashSet<>();
+        for (String number : numbersAsStrings) {
+            inputSet.add(Integer.parseInt(number));
+        }
         GamblingMachine gamblingMachine = new GamblingMachine();
-
-        Set<Integer> userNumbers1 = new HashSet<>(Set.of(1, 2, 3, 4, 5, 6));
-        int wins1 = gamblingMachine.howManyWins(userNumbers1);
-        assertEquals(6, wins1);
-
-        Set<Integer> userNumbers2 = new HashSet<>(Set.of(7, 8, 9, 10, 11, 12));
-        int wins2 = gamblingMachine.howManyWins(userNumbers2);
-        assertEquals(0, wins2);
-
-        Set<Integer> userNumbers3 = new HashSet<>(Set.of(1, 7, 14, 21, 28, 35));
-        int wins3 = gamblingMachine.howManyWins(userNumbers3);
-        assertTrue(wins3 > 0 && wins3 < 6);
+        assertThrows(InvalidNumbersException.class, () -> gamblingMachine.howManyWins(inputSet));
     }
 
-    @Test
-    void testInvalidNumbersException() {
+    @ParameterizedTest
+    @CsvFileSource(resources = "gamblingMachineNumbersOutOfScope.csv")
+    public void shouldThrowExceptionWhenNumberIsOutOfScope(String input) {
+        String[] numbersAsStrings = input.split("-");
+        Set<Integer> inputSet = new HashSet<>();
+        for (String number : numbersAsStrings) {
+            inputSet.add(Integer.parseInt(number));
+        }
         GamblingMachine gamblingMachine = new GamblingMachine();
-
-        Set<Integer> invalidNumbers1 = new HashSet<>(Set.of(1, 2, 3, 4, 5));
-        assertThrows(InvalidNumbersException.class, () -> gamblingMachine.howManyWins(invalidNumbers1));
-
-        Set<Integer> invalidNumbers2 = new HashSet<>(Set.of(1, 2, 3, 4, 5, 6, 7));
-        assertThrows(InvalidNumbersException.class, () -> gamblingMachine.howManyWins(invalidNumbers2));
-
-        Set<Integer> invalidNumbers3 = new HashSet<>(Set.of(1, 2, 3, 4, 5, 60));
-        assertThrows(InvalidNumbersException.class, () -> gamblingMachine.howManyWins(invalidNumbers3));
+        assertThrows(InvalidNumbersException.class, () -> gamblingMachine.howManyWins(inputSet));
+    }
+    @ParameterizedTest
+    @CsvFileSource(resources = "gamblingMachineGoodNumbers.csv")
+    public void shouldCalculateNumberOfWins(String input) throws InvalidNumbersException {
+        String[] numbersAsStrings = input.split("-");
+        Set<Integer> inputSet = new HashSet<>();
+        for (String number : numbersAsStrings) {
+            inputSet.add(Integer.parseInt(number));
+        }
+        GamblingMachine gamblingMachine = new GamblingMachine();
+        int howManyWins = gamblingMachine.howManyWins(inputSet);
+        assertTrue(howManyWins >= 0 && howManyWins <= 6);
     }
 }
