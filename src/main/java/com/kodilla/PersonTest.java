88910255a09c46d484887974ff8625ffd47dda0f
aleksma7 <143549406+aleksma7@users.noreply.github.com>
2023-12-31 06:38:37 +0100

    PersonTest.java

--- src/main/java/com/kodilla/PersonTest.java
+++ src/main/java/com/kodilla/PersonTest.java
@@ -2,108 +2,36 @@ package parametrized_tests;
 
 import com.kodilla.parametrized_tests.homework.Person;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PersonTest {
-    @Test
-    public void testGetBMIForNormalWeight() {
-        Person person = new Person(1.75, 70);
+    @ParameterizedTest
+    @MethodSource(value = "parametrized_tests.PersonTest#provideBmiAsAText")
+    public void shouldCalculateBMI(double height, int weight, String expected) {
+        Person person = new Person(height, weight);
+        assertEquals(expected, person.getBMI() );
 
-        assertEquals("Normal (healthy weight)", person.getBMI());
     }
 
-    @Test
-    public void testGetBMIForUnderweight() {
-        Person person = new Person(1.65, 45);
-
-        assertEquals("Underweight", person.getBMI());
-    }
-
-    @Test
-    public void testGetBMIForOverweight() {
-        Person person = new Person(1.75, 90);
-
-        assertEquals("Overweight", person.getBMI());
-    }
-    @Test
-    public void testGetBMIVerySeverelyUnderweight() {
-        Person person = new Person(1.60, 30);
-
-        assertEquals("Very severely underweight", person.getBMI());
-    }
-    @Test
-    public void testGetBMIUnderweight() {
-        Person person = new Person(1.70, 50);
-
-        assertEquals("Underweight", person.getBMI());
-    }
-
-    @Test
-    public void testGetBMIObeseClassI() {
-        Person person = new Person(1.80, 100);
-
-        assertEquals("Obese Class I (Moderately obese)", person.getBMI());
-    }
-
-    @Test
-    public void testGetBMIObeseClassIII() {
-        Person person = new Person(1.65, 110);
-
-        assertEquals("Obese Class III (Very severely obese)", person.getBMI());
-    }
-
-    public static class TestData {
-
-        public static Person underweightPerson() {
-            return new Person(1.60, 45);
-        }
-
-        public static Person normalWeightPerson() {
-            return new Person(1.75, 70);
-        }
-
-        public static Person overweightPerson() {
-            return new Person(1.75, 90);
-        }
-
-        public static Person obeseClassIIPerson() {
-            return new Person(1.70, 120);
-        }
-
-        public static Person obeseClassVPerson() {
-            return new Person(1.60, 200);
-        }
-    }
-
-    public static class PersonTestWithTestData {
-
-        @Test
-        public void testGetBMIForUnderweight() {
-            Person person = TestData.underweightPerson();
-
-            assertEquals("Underweight", person.getBMI());
-        }
-
-        @Test
-        public void testGetBMIForNormalWeight() {
-            Person person = TestData.normalWeightPerson();
-
-            assertEquals("Normal (healthy weight)", person.getBMI());
-        }
-
-        @Test
-        public void testGetBMIForOverweight() {
-            Person person = TestData.overweightPerson();
-
-            assertEquals("Overweight", person.getBMI());
-        }
-
-        @Test
-        public void testGetBMIForObeseClassII() {
-            Person person = TestData.obeseClassIIPerson();
-
-            assertEquals("Obese Class II (Severely obese)", person.getBMI());
-        }
+    private static Stream<Arguments> provideBmiAsAText() {
+        return Stream.of(
+                Arguments.of(1.7, 35, "Very severely underweight"),
+                Arguments.of(1.6, 204, "Obese Class VI (Hyper Obese)"),
+                Arguments.of(1.82, 51, "Severely underweight"),
+                Arguments.of(1.53, 42, "Underweight"),
+                Arguments.of(1.67, 64, "Normal (healthy weight)"),
+                Arguments.of(1.95, 110, "Overweight"),
+                Arguments.of(1.95, 126, "Obese Class I (Moderately obese)"),
+                Arguments.of(1.95, 141, "Obese Class II (Severely obese)"),
+                Arguments.of(1.73, 133, "Obese Class III (Very severely obese)"),
+                Arguments.of(1.65, 133, "Obese Class IV (Morbidly Obese)"),
+                Arguments.of(1.77, 186, "Obese Class V (Super Obese)")
+                );
     }
 }
